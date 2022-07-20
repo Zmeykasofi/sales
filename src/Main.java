0a130eeaa7d0fb77e0f154018cd761378c1ef660
Sophia Polyankina <zmeykasofi@mail.ru>
2022-07-20 18:42:28 +0700

    Int changed to int

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {2000, 5487, 10546, 1567, 10550};
+        long[] sales = {2000, 5487, 10546, 1567, 10550};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println("Наибольшая продажа составила " + salesManager.max() + " рублей.");
     }
